fe353801b5d868d4bf0e1cffc9a9d25e33d3394f
Daniel Sousa <sousa.dfs@gmail.com>
2023-04-30 14:01:16 -0300

    feat: add index

--- internal/commitlog/index.go
+++ internal/commitlog/index.go
@@ -1,1 +1,39 @@
 package commitlog
+
+import (
+	cmap "github.com/orcaman/concurrent-map/v2"
+)
+
+// index is an in-memory index of the commit log.
+type index struct {
+	items cmap.ConcurrentMap[string, *item]
+}
+
+// item is an index item.
+type item struct {
+	offset    uint64
+	timestamp int64
+	size      int64
+}
+
+// newIndex creates a new index.
+func newIndex() *index {
+	return &index{
+		items: cmap.New[*item](),
+	}
+}
+
+// Get returns an item from the index.
+func (i *index) Get(key string) (*item, bool) {
+	return i.items.Get(key)
+}
+
+// Set stores an item in the index.
+func (i *index) Set(key string, value *item) {
+	i.items.Set(key, value)
+}
+
+// Delete removes an item from the index.
+func (i *index) Delete(key string) {
+	i.items.Remove(key)
+}
--- internal/commitlog/index_test.go
+++ internal/commitlog/index_test.go
@@ -1,1 +1,48 @@
-package commitlog_test
+package commitlog
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var expectedIndexKeys = []string{
+	"hello world 1",
+	"hello world 2",
+	"hello world 3",
+}
+
+var expectedIndexItems = []*item{
+	{offset: 0, timestamp: 0, size: 25},
+	{offset: 25, timestamp: 0, size: 25},
+	{offset: 50, timestamp: 0, size: 25},
+}
+
+func TestIndexSetGetDelete(t *testing.T) {
+	idx := newIndex()
+	testSetGet(t, idx)
+	testDeleteGet(t, idx)
+}
+
+func testSetGet(t *testing.T, idx *index) {
+	t.Helper()
+	for i, key := range expectedIndexKeys {
+		idx.Set(key, expectedIndexItems[i])
+		item, exists := idx.Get(key)
+		require.Equal(t, expectedIndexItems[i], item)
+		require.True(t, exists)
+	}
+}
+
+func testDeleteGet(t *testing.T, idx *index) {
+	t.Helper()
+
+	item, exists := idx.Get(expectedIndexKeys[0])
+	require.Equal(t, expectedIndexItems[0], item)
+	require.True(t, exists)
+
+	idx.Delete(expectedIndexKeys[0])
+	item, exists = idx.Get(expectedIndexKeys[0])
+	require.Nil(t, item)
+	require.False(t, exists)
+}
